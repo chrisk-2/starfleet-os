9918a226e156b0b97cfc8fdb6bb18b458a472889
Christopher Paul Kelly <chrisk_2@live.com>
2025-08-29 17:24:52 -0500

    Update profiledef.sh

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,12 +1,31 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
 
-profile="starfleet"
-profile_desc="Starfleet OS Deployer ISO"
+# Allow role/version to be injected by env (defaults for local runs)
+: "${STARFLEET_ROLE:=server}"
+
+# If building from a tag like v0.3.2, prefer that; else fall back to date
+if [[ -n "${GITHUB_REF_NAME:-}" && "${GITHUB_REF_NAME}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  _ver="${GITHUB_REF_NAME#v}"
+else
+  _ver="$(date +%Y.%m.%d)"
+fi
+
+iso_name="starfleet-${STARFLEET_ROLE}"
+iso_label="STFL-${STARFLEET_ROLE^^}-$_ver"
+iso_publisher="Starfleet OS"
+iso_application="Starfleet ${STARFLEET_ROLE} live ISO"
+iso_version="${_ver}"
+
 install_dir="arch"
-bootmodes=('uefi-x64.systemd-boot.esp' 'bios.syslinux.mbr')
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.grub' 'uefi-ia32.grub' 'uefi-ia32.systemd-boot' 'uefi-x64.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=("-comp" "xz" "-b" "1M" "-Xbcj" "x86")
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '19')
 file_permissions=(
-  ["/root/.automated_script.sh"]="0:0:0755"
+  [/root]=0:0:750
+  [/root/.automated_script.sh]=0:0:755
+  [/usr/local/bin/starfleet-firstboot]=0:0:755
 )
